626fc247c4b2c24f6459414d36071c60e60b5f76
Christian Traxler <traxler.lc@gmail.com>
2021-09-17 23:00:49 -0400

    create base api

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+/*
 import cors from 'cors';
 import express from 'express';
 import expressGraphQL from 'express-graphql';
@@ -11,7 +11,6 @@ import {
     GraphQLNonNull,
     GraphQLBoolean
 } from 'graphql';
-//const { ApolloServer } = require("apollo-server-express");
 
 
 import { TeamType, GameType, DivisionType, ConferenceType } from "./types.js"
@@ -105,7 +104,7 @@ app.get('*', (req, res) => {
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-
+*/
 /*
 const server = new ApolloServer({ schema });
 
@@ -115,3 +114,31 @@ server.applyMiddleware({ app });
 
 app.listen({ port: PORT });
 */
+
+
+import express from 'express';
+import graphqlHTTP from 'express-graphql';
+import { buildSchema } from 'graphql';
+
+// Construct a schema, using GraphQL schema language
+var schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+// The root provides a resolver function for each API endpoint
+var root = {
+  hello: () => {
+    return 'Hello world!';
+  },
+};
+
+var app = express();
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
+app.listen(4000);
+console.log('Running a GraphQL API server at http://localhost:4000/graphql');
